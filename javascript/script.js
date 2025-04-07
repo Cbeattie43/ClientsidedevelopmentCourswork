4f33dd87d22cd90bb5423111d6e0477e2dbff328
JAY55554 <Davidson-J27@ulster.ac.uk>
2025-04-07 14:37:44 +0100

    sf

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+
+function updateDateTime() {
+    const now = new Date();
+    document.getElementById('date-time').textContent = now.toLocaleString();
+}
+setInterval(updateDateTime, 1000);
+updateDateTime();
